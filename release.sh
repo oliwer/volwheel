5f1011294f6b1870b820bb5de7a17d67a3cebf93
Olivier Duclos <odc@cpan.org>
2018-10-05 11:21:31 +0200

    Modernize release.sh

--- release.sh
+++ release.sh
@@ -1,19 +1,18 @@
-#!/bin/sh
+#!/bin/sh -eu
 
 # VolWheel - set the volume with your mousewheel
 # Author : Olivier Duclos <olivier.duclos gmail.com>
 
-cd `pwd`
+cd "$(dirname $0)"
 
 PACKAGE=volwheel
 VERSION=$(head -n1 ChangeLog | cut -d "v" -f2)
 DIR=$HOME/$PACKAGE-$VERSION
 
+rm -rf $DIR
 mkdir $DIR
 cp -R * $DIR/
 rm $DIR/release.sh
-rm -rf $DIR/*/.svn
-rm -rf $DIR/*/*/.svn
 cd ~
 tar cvzf $PACKAGE-$VERSION.tar.gz $PACKAGE-$VERSION
 rm -rf $DIR
